04d594671e77d5f28f47a3f087d132e200b0814f
AliceBabyNana <41255555+AliceBabyNana@users.noreply.github.com>
2018-08-26 21:50:53 +0800

    update

--- content/js/app.js
+++ content/js/app.js
@@ -1,11 +1,27 @@
-onload=()=>{
-    $("#content .post-body img").SetImgResponsive();
-};
+$(function () {
 
-
+    InitBlogSetting();
+});
 
 
 
 $.fn.SetImgResponsive = function () {
     $(this).addClass('img-responsive');
 };
+
+
+
+const InitBlogSetting = () => {
+    //set sidebar icon dsec 
+    $(".my-item>a").each((i, v) => {
+        const str = $(v).data('tip');
+        $(v).hover(() => {
+            layer.tips(str, v, {
+                tips: [1, '#ef8585'] //还可配置颜色
+            });
+        });
+    });
+    //set article content img responsive
+    $("#content .post-body img").SetImgResponsive();
+}
+
